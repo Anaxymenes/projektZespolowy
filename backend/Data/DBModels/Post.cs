ba6959c56d156a2621e4eaba30b2c74638ae63aa
Artur Czopek <artur.czopek@contractors.roche.com>
2018-04-23 21:43:54 +0200

    DbModels update
    Fix Post model and add PostType model

--- backend/Data/DBModels/Post.cs
+++ backend/Data/DBModels/Post.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Data.DBModels
 {
     public class Post : Entity
     {
+        [Column(TypeName = "datetime2")]
         public DateTime Date { get; set; }
-        public string PostType { get; set; }
+        public string Content { get; set; }
 
+        public PostType PostType { get; set; }
         public Account Author { get; set; }
         public ICollection<Picture> Pictures { get; set; }
     }
---
+++ backend/Data/DBModels/PostType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DBModels
+{
+    public class PostType : Entity{
+        public string Name { get; set; }
+    }
+}
