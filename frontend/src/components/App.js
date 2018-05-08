adc5383147eeecae92a77fe09f84d15f55354e21
PiotrB175 <piotrb.175@gmail.com>
2018-05-08 18:04:13 +0200

    Add images

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './common/Header';
 import Routes from './Routes';
+import Background from '../img/bg.jpg';
+
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div style={{backgroundImage: "url(" + Background + ")", 
+            backgroundSize: 'cover',
+            overflow: 'hidden',
+            "height" : "100%",}}>
+            {/* <img src={ Background } />  */}
             <Header />
             <Routes />
             </div>
