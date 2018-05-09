6a5230322cd6f0ac804eb605c572ffbfc0e0ff21
Artur Czopek <artur.czopek@contractors.roche.com>
2018-05-09 19:24:02 +0200

    Hobby Controller

---
+++ backend/WebApi/Controllers/HobbyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HobbyController : Controller{
+
+        private IHobbyService _service;
+
+
+    }
+}
