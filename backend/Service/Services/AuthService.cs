7a8bc4df27c7f3128624d207ff6816d06ac5be4b
Artur Czopek <artur.czopek@contractors.roche.com>
2018-05-24 01:34:56 +0200

    Salt Gen

--- backend/Service/Services/AuthService.cs
+++ backend/Service/Services/AuthService.cs
@@ -1,10 +1,12 @@
 ﻿using Data.DTO;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Service.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Service.Services
@@ -43,5 +45,26 @@ namespace Service.Services
             jwTBearerToken.Expires = token.ValidTo;
             return jwTBearerToken;
         }
+
+        public byte[] GetSalt() {
+            byte[] salt = new byte[32 / 8];
+            using (var rng = RandomNumberGenerator.Create()) {
+                rng.GetBytes(salt);
+            }
+            string result = Convert.ToBase64String(salt);
+            return Encoding.ASCII.GetBytes(result);
+        }
+
+        public string GetHashedPassword(string password, byte[] salt) {
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    password: password,
+                    salt: salt,
+                    prf: KeyDerivationPrf.HMACSHA1,
+                    iterationCount: 10000,
+                    numBytesRequested: 256 / 8
+                ));
+            return hashedPassword;
+        }
+
     }
 }
