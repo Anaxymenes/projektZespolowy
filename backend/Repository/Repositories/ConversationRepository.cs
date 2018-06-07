673249f20b8378eb4b19be454f8eb5e20f3320c6
MateuszBogacki <manti.2@vp.pl>
2018-06-07 23:43:54 +0200

    Conversation base

---
+++ backend/Repository/Repositories/ConversationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Repositories
+{
+    public class ConversationRepository : IConversationRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public ConversationRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ backend/Service/Services/ConversationService.cs
@@ -0,0 +1,17 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class ConversationService : IConversationService
+    {
+        private readonly IConversationRepository _conversationRepository;
+
+        public ConversationService(IConversationRepository conversationRepository)
+        {
+            _conversationRepository = conversationRepository;
+        }
+    }
+}
